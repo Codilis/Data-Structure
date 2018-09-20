f5f87dfbf4180cf7b16c0cf1d0e12bd46d43d9d4
Palash Jain <palash.j22@gmail.com>
2018-09-20 18:35:45 +0530

    code corrected

--- Linked_list/bracket related questions.cpp
+++ Linked_list/bracket related questions.cpp
@@ -3,25 +3,67 @@
 //
 
 #include <iostream>
+using namespace std;
+
 
 struct node {
     char bracket;
     node * next;
 };
-using namespace std;
+
+node *insert(node *head, char value){
+    node * temp = new node;
+    temp->next = head;
+    head = temp;
+    temp->bracket = value;
+    return head;
+
+}
+
+
+bool correct_brackets(char brackets[100], int n) {
+    node *head = new node;
+    head = NULL;
+    bool ans = false;
+    for (int i = 0; i < n; ++i) {
+        if (brackets[i] == '{' || brackets[i] == '[' || brackets[i] == '(')
+            head = insert(head, brackets[i]);
+        else {
+            if (head == NULL) {
+                ans = false;
+                break;
+            }
+            char x = head->bracket;
+            head = head->next;
+            if(brackets[i] == '}' && x == '{')
+                ans = true;
+            else if (brackets[i]==')' && x == '(')
+            ans = true;
+            else if (brackets[i]==']' && x == '[')
+                ans = true;
+            else {
+                ans = false;
+                break;
+            }
+        }
+    }
+    if (head != NULL)
+        ans = false;
+    return ans;
+}
+
 int main(){
     char brackets[100];
     int n;
-    node *head = new node;
-    head = NULL;
+
     cin>>n;
     for (int i = 0; i < n; ++i) {
         cin>>brackets[i];
     }
+    if (correct_brackets(brackets, n))
+        cout<<"CORRECT";
+    else
+        cout<<"INCORRECT";
 
-    for (int i = 0; i < n; ++i) {
-        if (brackets[i] == '{')
-
-    }
 
 }
